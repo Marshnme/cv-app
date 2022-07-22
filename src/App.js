1db0970aad60b590588f45f74e3592dd19165138
Marshnme <jildsmiles@yahoo.com>
2022-07-22 00:55:02 -0400

    basic state created

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { name: '' };
+		this.state = {
+			editToggle: false,
+			generalInfo: [],
+			education: [],
+			workExp: [],
+		};
 	}
 	render() {
 		return (
