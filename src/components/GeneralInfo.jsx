c7c8af1acf5b69d1b7d3b432c27835a2179e5120
Marshnme <jildsmiles@yahoo.com>
2022-07-28 13:43:22 -0400

    general info changed to functional components and working properly

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,92 +1,80 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import EditIcon from '@mui/icons-material/Edit';
 import '../styles/GeneralInfo.css'
 
-class GeneralInfo extends React.Component{
-    constructor(props) {
-        super(props)
+const GeneralInfo = (props) => {
+    const [editName, setEditName] = useState(false)
+    const [editContact, setEditContact] = useState(false)
+    const [nameField,setNameField] = useState(props.info.userName)
+    const [emailField,setEmailField] = useState(props.info.email)
+    const [phoneField,setPhoneField] = useState(props.info.phone)
+    const [websiteField,setWebsiteField] = useState(props.info.website)
 
-        this.state = {
-            editName:false,
-            editContact: false,
-            nameField: this.props.info.userName,
-            emailField: this.props.info.email,
-            phoneField: this.props.info.phone,
-            websiteField: this.props.info.website,
-        }
 
-        this.handleNameEdit = this.handleNameEdit.bind(this)
-        this.handleNameChange = this.handleNameChange.bind(this)
-        this.handleContactEdit = this.handleContactEdit.bind(this)
-        this.handleContactChange = this.handleContactChange.bind(this)
 
+
+    function handleNameEdit() {
+        editName ? setEditName(false) : setEditName(true)
     }
-
-    handleNameEdit() {
-        this.state.editName ? this.setState({ editName: false }) : this.setState({ editName: true })
-    }
-    handleNameChange(e) {
+    function handleNameChange(e) {
         e.preventDefault()
         
-        this.setState({nameField:e.target.value})
+        setNameField(e.target.value)
     }
     
-    handleContactEdit() {
-        this.state.editContact ? this.setState({ editContact: false }) : this.setState({ editContact: true })
+    function handleContactEdit() {
+        editContact ? setEditContact(false) : setEditContact(true)
     }
 
-    handleContactChange(e) {
+    function handleContactChange(e) {
         e.preventDefault()
         
         if (e.target.id === 'email') {
-            this.setState({emailField:e.target.value})
+            setEmailField(e.target.value)
         } else if (e.target.id === 'phone') {
-            this.setState({phoneField:e.target.value})
+            setPhoneField(e.target.value)
         } else if (e.target.id === 'website') {
-            this.setState({websiteField:e.target.value})
+            setWebsiteField(e.target.value)
         }
     }
 
    
     
 
-    render() {
-        let { edit, info ,handleNameSubmit,handleContactSubmit} = this.props
-        let {editName, editContact, nameField,emailField,phoneField,websiteField} = this.state
+    
         return (
             <div className='general-info-container'>
                 <div className="name">
-                    <form  onSubmit={handleNameSubmit.bind(this,nameField,this.handleNameEdit)}>
-                        {edit ? editName ? <><input type='text' onChange={this.handleNameChange} value={nameField}></input> <button type='submit'>Apply</button></> : <h3>{info.userName}
-                            <EditIcon className='cursor-pointer' fontSize='small' onClick={this.handleNameEdit}></EditIcon></h3> : <h3>{info.userName}</h3>}
+                    <form  onSubmit={props.handleNameSubmit.bind(this,nameField,handleNameEdit)}>
+                        {props.edit ? editName ? <><input type='text' onChange={handleNameChange} value={nameField}></input> <button type='submit'>Apply</button></> : <h3>{props.info.userName}
+                            <EditIcon className='cursor-pointer' fontSize='small' onClick={handleNameEdit}></EditIcon></h3> : <h3>{props.info.userName}</h3>}
                     </form>
                 </div>
                 <div className="contact-info">
-                    {edit ?
+                    {props.edit ?
                         editContact ? <>
-                            <form onSubmit={handleContactSubmit.bind(this,emailField,phoneField,websiteField,this.handleContactEdit)}>
-                                <input id='email' type='email' onChange={this.handleContactChange} value={emailField}></input>
-                                <input id='phone' type='text' onChange={this.handleContactChange} value={phoneField}></input>
-                                <input id='website' type='text' onChange={this.handleContactChange} value={websiteField}></input>
+                            <form onSubmit={props.handleContactSubmit.bind(this,emailField,phoneField,websiteField,handleContactEdit)}>
+                                <input id='email' type='email' onChange={handleContactChange} value={emailField}></input>
+                                <input id='phone' type='text' onChange={handleContactChange} value={phoneField}></input>
+                                <input id='website' type='text' onChange={handleContactChange} value={websiteField}></input>
                                 <button type='submit'>Apply</button>
                             </form>
                         </>
                         :
                             
                         <>
-                            <p>{info.email}<EditIcon className='cursor-pointer' fontSize='small' onClick={this.handleContactEdit}></EditIcon></p>
-                            <p>{info.phone}</p>
-                            <p>{info.website}</p>
+                            <p>{props.info.email}<EditIcon className='cursor-pointer' fontSize='small' onClick={handleContactEdit}></EditIcon></p>
+                            <p>{props.info.phone}</p>
+                            <p>{props.info.website}</p>
                         </> :
                         <>
-                            <p>{info.email}</p>
-                            <p>{info.phone}</p>
-                            <p>{info.website}</p>
+                            <p>{props.info.email}</p>
+                            <p>{props.info.phone}</p>
+                            <p>{props.info.website}</p>
                         </>}
                 </div>
             </div>
         )
-    }
 }
 
 export default GeneralInfo
